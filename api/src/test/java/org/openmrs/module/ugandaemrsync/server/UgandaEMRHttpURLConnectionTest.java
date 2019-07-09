a77c162a642ef7db5cfae14e9c938cb82a00efbc
Ezekiel Chiteri <ezekiel.chiteri@ucglobalprograms.orggit>
2019-07-09 11:37:45 +0300

    Removed wildcards from library imports

--- api/src/test/java/org/openmrs/module/ugandaemrsync/server/UgandaEMRHttpURLConnectionTest.java
+++ api/src/test/java/org/openmrs/module/ugandaemrsync/server/UgandaEMRHttpURLConnectionTest.java
@@ -3,7 +3,7 @@ package org.openmrs.module.ugandaemrsync.server;
 import org.apache.poi.hssf.record.formula.functions.True;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UgandaEMRHttpURLConnectionTest {
 
