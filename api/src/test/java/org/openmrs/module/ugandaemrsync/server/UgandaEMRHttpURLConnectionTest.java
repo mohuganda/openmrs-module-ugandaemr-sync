d493ee58332b1fe896d843f073c166c10f969e01
Ezekiel Chiteri <ezekiel.chiteri@ucglobalprograms.orggit>
2019-07-17 10:07:34 +0300

    Modified test to assertTrue instead of equal Added test to check server unavailability

--- api/src/test/java/org/openmrs/module/ugandaemrsync/server/UgandaEMRHttpURLConnectionTest.java
+++ api/src/test/java/org/openmrs/module/ugandaemrsync/server/UgandaEMRHttpURLConnectionTest.java
@@ -3,20 +3,23 @@ package org.openmrs.module.ugandaemrsync.server;
 import org.apache.poi.hssf.record.formula.functions.True;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class UgandaEMRHttpURLConnectionTest {
 
     @Test
     public void isConnectionAvailable() {
         UgandaEMRHttpURLConnection ugandaEMRHttpURLConnection = new UgandaEMRHttpURLConnection();
-        assertEquals(
-                ugandaEMRHttpURLConnection.isConnectionAvailable(), true);
+        assertTrue(ugandaEMRHttpURLConnection.isConnectionAvailable());
     }
     @Test
     public void isServerAvailable() {
         UgandaEMRHttpURLConnection ugandaEMRHttpURLConnection = new UgandaEMRHttpURLConnection();
-        assertEquals(
-                ugandaEMRHttpURLConnection.isServerAvailable("http://mirth-tcp.globalhealthapp.net"), true);
+        assertTrue(ugandaEMRHttpURLConnection.isServerAvailable("http://mirth-tcp.globalhealthapp.net"));
+    }
+    @Test
+    public void thisServerDoesNotExist() {
+        UgandaEMRHttpURLConnection ugandaEMRHttpURLConnection = new UgandaEMRHttpURLConnection();
+        assertFalse(ugandaEMRHttpURLConnection.isServerAvailable("http://no-server.exists.ug"));
     }
 }
