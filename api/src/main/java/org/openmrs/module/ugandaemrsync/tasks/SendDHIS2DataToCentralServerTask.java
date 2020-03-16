04abac080b5a319e023b75efc262baf66670508f
Lubwama Samuel <lubwamasamuel3@gmail.com>
2020-03-16 11:17:28 +0300

    improving messages that are sent to the interface

--- api/src/main/java/org/openmrs/module/ugandaemrsync/tasks/SendDHIS2DataToCentralServerTask.java
+++ api/src/main/java/org/openmrs/module/ugandaemrsync/tasks/SendDHIS2DataToCentralServerTask.java
@@ -56,54 +56,78 @@ public class SendDHIS2DataToCentralServerTask extends AbstractTask  {
 	protected ReportDefinitionService reportingReportDefinitionService;
 
 
-	public void execute() {
-		Map map = new HashMap();
-		int responseCode = 0;
-		String baseUrl = ugandaEMRHttpURLConnection.getBaseURL(syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_URL));
-		if (isBlank(syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_URL))) {
-			log.error("DHIS 2 server URL is not set");
-			ugandaEMRHttpURLConnection
-					.setAlertForAllUsers("DHIS 2 server URL is not set please go to admin then Settings then Ugandaemrsync and set it");
-			return;
-		}
+    public void execute() {
+        Map map = new HashMap();
+        ObjectMapper objectMapper = new ObjectMapper();
+        int responseCode = 0;
+        String baseUrl = ugandaEMRHttpURLConnection.getBaseURL(syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_URL));
+        if (isBlank(syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_URL))) {
+            log.error("DHIS 2 server URL is not set");
 
-		//Check internet connectivity
-		if (!ugandaEMRHttpURLConnection.isConnectionAvailable()) {
-			return;
-		}
+            ugandaEMRHttpURLConnection.setAlertForAllUsers("DHIS 2 server URL is not set please go to admin then Settings then Ugandaemrsync and set it");
 
-		//Check destination server availability
-		if (!ugandaEMRHttpURLConnection.isServerAvailable(baseUrl)) {
-			return;
-		}
+            try {
+                map.put("responseCode", responseCode);
+                map.put("responseMessage", "DHIS 2 server URL is not set");
+                serverResponseObject = SimpleObject.create("message", objectMapper.writeValueAsString(map));
+            } catch (IOException e) {
+                log.error("error creating message to interface", e);
+            }
+            log.error("DHIS 2 server URL is not set");
+            return;
+        }
 
-		log.error("Sending DHIS2 data to central server ");
-		String bodyText = new String(this.data);
-		try {
-			HttpResponse httpResponse = ugandaEMRHttpURLConnection.httpPost(syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_URL), bodyText, syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_USERNAME), syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_PASSWORD));
-			responseCode = httpResponse.getStatusLine().getStatusCode();
-			String responseMessage = httpResponse.getStatusLine().getReasonPhrase();
-			if ((responseCode == 200 || responseCode == 201)) {
-				InputStream inputStreamReader = httpResponse.getEntity().getContent();
-				map = getMapOfResults(inputStreamReader, responseCode);
-			} else {
-				map.put("responseCode", responseCode);
-				log.info(responseMessage);
-			}
-			map.put("responseMessage", responseMessage);
-		}catch (IOException e){
-			log.error(e);
-		}
+        //Check internet connectivity
+        if (!ugandaEMRHttpURLConnection.isConnectionAvailable()) {
+            map.put("responseCode", responseCode);
+            map.put("responseMessage", "Failed to connect to the internet. Check connection");
+            try {
+                serverResponseObject = SimpleObject.create("message", objectMapper.writeValueAsString(map));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
 
-		ObjectMapper objectMapper=new ObjectMapper();
-		try {
-			serverResponseObject = SimpleObject.create("message", objectMapper.writeValueAsString(map));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		serverResponseObject.put("responsedata",responseCode);
+        //Check destination server availability
+        if (!ugandaEMRHttpURLConnection.isServerAvailable(baseUrl)) {
+            map.put("responseCode", responseCode);
+            map.put("responseMessage", "Server Not Available");
 
-	}
+            try {
+                serverResponseObject = SimpleObject.create("message", objectMapper.writeValueAsString(map));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
+        log.error("Sending DHIS2 data to central server ");
+        String bodyText = new String(this.data);
+        try {
+            HttpResponse httpResponse = ugandaEMRHttpURLConnection.httpPost(syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_URL), bodyText, syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_USERNAME), syncGlobalProperties.getGlobalProperty(GP_DHIS2_SERVER_PASSWORD));
+            responseCode = httpResponse.getStatusLine().getStatusCode();
+            String responseMessage = httpResponse.getStatusLine().getReasonPhrase();
+            if ((responseCode == 200 || responseCode == 201)) {
+                InputStream inputStreamReader = httpResponse.getEntity().getContent();
+                map = getMapOfResults(inputStreamReader, responseCode);
+            } else {
+                map.put("responseCode", responseCode);
+                log.info(responseMessage);
+            }
+            map.put("responseMessage", responseMessage);
+        } catch (IOException e) {
+            log.error(e);
+        }
+
+
+        try {
+            serverResponseObject = SimpleObject.create("message", objectMapper.writeValueAsString(map));
+            serverResponseObject.put("responseCode", responseCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 	public Map getMapOfResults(InputStream inputStreamReader, int responseCode) throws IOException {
 		Map map = new HashMap();
