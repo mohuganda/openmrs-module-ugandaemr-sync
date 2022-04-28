6c5372ed36edf1e6252af4b188daa36937db420a
dbaluku <juniorderrick18@gmail.com>
2022-04-28 20:11:35 +0300

    Added sync log the right way

--- api/src/main/java/org/openmrs/module/ugandaemrsync/server/TaskType.java
+++ api/src/main/java/org/openmrs/module/ugandaemrsync/server/TaskType.java
@@ -2,10 +2,16 @@ package org.openmrs.module.ugandaemrsync.server;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.GlobalProperty;
+import org.openmrs.*;
+import org.openmrs.api.APIException;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.ugandaemrsync.api.UgandaEMRSyncService;
-import org.openmrs.module.ugandaemrsync.model.SyncTaskType;
+import org.openmrs.module.ugandaemrsync.model.*;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
 public class TaskType {
 
@@ -13,7 +19,6 @@ public class TaskType {
 	}
 	
 	protected Log log = LogFactory.getLog(TaskType.class);
-	
 
 	
 	public GlobalProperty setGlobalProperty(String property, String propertyValue) {
@@ -27,6 +32,20 @@ public class TaskType {
 	
 	public SyncTaskType getSyncTaskType(String uuid) {
 		return Context.getService(UgandaEMRSyncService.class).getSyncTaskTypeByUUID(uuid);
+	}
+
+	public void saveSyncTaskForSyncTaskType(SyncTaskType syncTaskType){
+		SyncTask newSyncTask = new SyncTask();
+		newSyncTask.setDateSent(new Date());
+		newSyncTask.setCreator(Context.getUserService().getUser(1));
+		newSyncTask.setSentToUrl(syncTaskType.getUrl());
+		newSyncTask.setRequireAction(true);
+		newSyncTask.setActionCompleted(false);
+		newSyncTask.setSyncTask(syncTaskType.getName());
+		newSyncTask.setStatusCode(200);
+		newSyncTask.setStatus("SUCCESS");
+		newSyncTask.setSyncTaskType(syncTaskType);
+		Context.getService(UgandaEMRSyncService.class).saveSyncTask(newSyncTask);
 	}
 	
 }
