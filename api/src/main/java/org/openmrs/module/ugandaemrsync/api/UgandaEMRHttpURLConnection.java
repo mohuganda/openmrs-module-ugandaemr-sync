d3512ff7f3ae26918df8bed76e2415e210a035b1
dbaluku <juniorderrick18@gmail.com>
2022-07-14 14:32:12 +0300

    removed disable ssl certificates

--- api/src/main/java/org/openmrs/module/ugandaemrsync/api/UgandaEMRHttpURLConnection.java
+++ api/src/main/java/org/openmrs/module/ugandaemrsync/api/UgandaEMRHttpURLConnection.java
@@ -466,28 +466,6 @@ public class UgandaEMRHttpURLConnection {
         return true;
     }
 
-    public void disableSSLCertificatesChecks() throws Exception {
-        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-        final TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    @Override
-                    public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-
-                    }
-
-                    @Override
-                    public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-
-                    }
-
-                    @Override
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-                }
-        };
-    }
-
 
     public static CloseableHttpClient createAcceptSelfSignedCertificateClient()
             throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
